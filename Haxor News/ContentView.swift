75a61ce862e2969b3afc66e5dc9fa3610922b85c
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-10 00:11:56 -0400

    Use array for List

--- Haxor News/ContentView.swift
+++ Haxor News/ContentView.swift
@@ -11,9 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            List {
-                Text("Hello, World!")
-                Text("Good bye, World!")
+            List(posts)  { post in
+                Text(post.title)
             }
         .navigationBarTitle("Haxor News")
         }
@@ -25,3 +24,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Noroc")
+]
+
