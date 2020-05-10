bd58695271940f4b8b7ce85c60d5b9c284aef963
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-10 02:08:10 -0400

    Refactor

---
+++ Haxor News/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  Haxor News
+//
+//  Created by Iurie Guzun on 2020-05-10.
+//  Copyright © 2020 Iurie Guzun. All rights reserved.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView{
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
